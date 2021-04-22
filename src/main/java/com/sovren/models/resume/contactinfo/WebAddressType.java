ed7941b9f4d21b749e9537e211e2ec4f2602cef6
JW Wesson <dev4@sovren.com>
2021-04-22 16:25:13 -0500

    added new contact info WebAddress types (social media)

--- src/main/java/com/sovren/models/resume/contactinfo/WebAddressType.java
+++ src/main/java/com/sovren/models/resume/contactinfo/WebAddressType.java
@@ -8,23 +8,68 @@ package com.sovren.models.resume.contactinfo;
 /** A type of {@link WebAddress}. These are useful instead of magic strings.*/
 public class WebAddressType {
 
+    /** An unknown internet handle/URL (the platform/website/app was not specified)*/
+    public static WebAddressType Unknown = new WebAddressType("Unknown");
+    
     /** A personal website URL*/
     public static WebAddressType PersonalWebsite = new WebAddressType("PersonalWebsite");
 
     /** A LinkedIn URL*/
     public static WebAddressType LinkedIn = new WebAddressType("LinkedIn");
 
-    /** A candidate's Twitter handle*/
+    /** A Twitter handle*/
     public static WebAddressType TwitterHandle = new WebAddressType("Twitter");
 
-    /** A candidate's Facebook profile URL*/
+    /** A Facebook profile URL*/
     public static WebAddressType Facebook = new WebAddressType("Facebook");
 
-    /** A candidate's Instagram username*/
+    /** An Instagram username*/
     public static WebAddressType Instagram = new WebAddressType("Instagram");
 
-    /** A candidate's ICQ username*/
+    /** An ICQ username*/
     public static WebAddressType ICQ = new WebAddressType("ICQ");
+    
+    /** A Quora username */
+    public static WebAddressType Quora = new WebAddressType("Quora");
+
+    /** A Skype username/URL */
+    public static WebAddressType Skype = new WebAddressType("Skype");
+
+    /** A WeChat username */
+    public static WebAddressType WeChat = new WebAddressType("WeChat");
+
+    /** A QQ username/number */
+    public static WebAddressType QQ = new WebAddressType("QQ");
+
+    /** A Telegraph username */
+    public static WebAddressType Telegraph = new WebAddressType("Telegraph");
+
+    /** A WhatsApp username/number */
+    public static WebAddressType WhatsApp = new WebAddressType("WhatsApp");
+
+    /** A Telegram username */
+    public static WebAddressType Telegram = new WebAddressType("Telegram");
+
+    /** A MeWe username/URL */
+    public static WebAddressType MeWe = new WebAddressType("MeWe");
+
+    /** A Parler username */
+    public static WebAddressType Parler = new WebAddressType("Parler");
+
+    /** A Gab username */
+    public static WebAddressType Gab = new WebAddressType("Gab");
+
+    /** A Reddit username/URL */
+    public static WebAddressType Reddit = new WebAddressType("Reddit");
+
+    /** A GitHub username/URL */
+    public static WebAddressType GitHub = new WebAddressType("GitHub");
+
+    /** A Signal username/number */
+    public static WebAddressType Signal = new WebAddressType("Signal");
+
+    /** A Stack Overflow username/URL */
+    public static WebAddressType StackOverflow = new WebAddressType("StackOverflow");
 
     /** The raw string value */
     public String Value;
