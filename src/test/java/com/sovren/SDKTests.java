2dddbdc425cc03f54f255463f246fab891b96846
JW Wesson <dev4@sovren.com>
2021-07-09 14:05:20 -0500

    fix tests

--- src/test/java/com/sovren/SDKTests.java
+++ src/test/java/com/sovren/SDKTests.java
@@ -37,7 +37,7 @@ public class SDKTests extends TestBase {
         }
         catch (SovrenException e){
             assertEquals(500, e.HttpStatusCode);
-            assertEquals("connect timed out", e.getMessage());
+            assertEquals("connect timed out", e.getMessage().toLowerCase());
         }
     }
     
