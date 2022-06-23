929e587d9ea154bfb1301db0608d8ebfef262196
JW Wesson <wesson@textkernel.nl>
2022-06-23 14:41:21 -0500

    change what Group.Percent means

--- src/main/java/com/sovren/models/skills/ProfessionGroup.java
+++ src/main/java/com/sovren/models/skills/ProfessionGroup.java
@@ -12,7 +12,7 @@ public class ProfessionGroup {
         /** Id of the profession group. */
         public String Id;
 
-        /** Precent of parent described by this profession group. All values under a parent will add up to 100%. */
+        /** Percent of overall document described by this profession group. All groups across all classes will add up to 100%.*/
         public double Percent;
 
         /** Array of normalized skills associated to this profession group. */
