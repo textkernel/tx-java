4205b5ff4b29de3b3e3d4fbff364f47619173bc9
Zach Ebner <ebner@textkernel.nl>
2023-01-27 16:08:14 -0600

    adding onet version to Profession Normalization request

---
+++ src/main/java/com/sovren/models/api/dataenrichmentservices/professions/ONETVersion.java
@@ -0,0 +1,20 @@
+// Copyright © 2020 Sovren Group, Inc. All rights reserved.
+// This file is provided for use by, or on behalf of, Sovren licensees
+// within the terms of their license of Sovren products or Sovren customers
+// within the Terms of Service pertaining to the Sovren SaaS products.
+
+package com.sovren.models.api.dataenrichmentservices.professions;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+*  Available ONET Versions
+*/
+public enum ONETVersion {
+    /**  ONET 2010 */
+    @SerializedName("2010")
+    ONET2010,
+     /**  ONET 2010 */
+    @SerializedName("2019")
+    ONET2019
+}
