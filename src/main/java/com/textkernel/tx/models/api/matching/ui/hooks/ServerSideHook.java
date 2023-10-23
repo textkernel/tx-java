52654f3413c464f558dd05a0c16f129f7caafced
JW Wesson <wesson@textkernel.nl>
2023-10-23 16:42:14 -0500

    update copyrights

--- src/main/java/com/textkernel/tx/models/api/matching/ui/hooks/ServerSideHook.java
+++ src/main/java/com/textkernel/tx/models/api/matching/ui/hooks/ServerSideHook.java
@@ -1,7 +1,7 @@
-// Copyright © 2020 Sovren Group, Inc. All rights reserved.
-// This file is provided for use by, or on behalf of, Sovren licensees
-// within the terms of their license of Sovren products or Sovren customers
-// within the Terms of Service pertaining to the Sovren SaaS products.
+// Copyright © 2023 Textkernel BV. All rights reserved.
+// This file is provided for use by, or on behalf of, Textkernel licensees
+// within the terms of their license of Sovren products or Textkernel customers
+// within the Terms of Service pertaining to the Textkernel SaaS products.
 
 package com.textkernel.tx.models.api.matching.ui.hooks;
 
