5050148f40ae2fadffeb449f52e765947fba295f
JW Wesson <wesson@textkernel.nl>
2024-03-01 13:33:48 -0600

    add some missing docs

--- src/main/java/com/textkernel/tx/models/api/parsing/FlexRequestDataType.java
+++ src/main/java/com/textkernel/tx/models/api/parsing/FlexRequestDataType.java
@@ -4,9 +4,14 @@ package com.textkernel.tx.models.api.parsing;
  * Possible data types for FlexRequests
  */
 public enum FlexRequestDataType {
+    /** Text/string response expected */
     Text,
+    /** Numeric response expected */
     Numeric,
+    /** Boolean response expected */
     Bool,
+    /** List of text responses expected */
     List,
+    /** Text response should be from a provided list of options. See {@link FlexRequest#EnumerationValues} */
     Enumeration
 }
