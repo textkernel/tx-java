984d97173c678156b843f41ffe213558981229c4
JW Wesson <wesson@textkernel.nl>
2024-05-08 10:54:36 -0500

    update okhttp and set longer timeout

--- src/main/java/com/textkernel/tx/TxClient.java
+++ src/main/java/com/textkernel/tx/TxClient.java
@@ -84,6 +84,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 /**
@@ -173,6 +174,9 @@ public class TxClient {
                     return chain.proceed(request);
                 }
             })
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .writeTimeout(10, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
             .build();
     }
 
